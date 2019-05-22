c135df37da58bd7bd7a60eff2bc0deed1850cd40
Darya Razumova <d.razumova@yandex.ru>
2019-05-22 16:51:47 +0300

    client

--- FTP/src/main/java/com/task/ftp/Client.java
+++ FTP/src/main/java/com/task/ftp/Client.java
@@ -2,20 +2,84 @@ package com.task.ftp;
 
 import java.io.*;
 import java.net.*;
+import java.nio.charset.*;
+import java.util.*;
 
 public class Client {
 
-    int connect(String hostName, int portNumber) throws IOException {
+    private int port;
+    private String hostName;
+
+    void connect(String hostName, int portNumber) throws IOException {
+        this.hostName = hostName;
         try (var socket = new Socket(hostName, portNumber);
              var in = new DataInputStream(socket.getInputStream());
              var out = new DataOutputStream(socket.getOutputStream())) {
 
+            out.writeChars("Hello");
+
         }
     }
 
-    int disconnect() {
-
+    void disconnect() throws IOException {
+        try (var socket = new Socket(hostName, port);
+             var in = new DataInputStream(socket.getInputStream());
+             var out = new DataOutputStream(socket.getOutputStream())) {
+            out.writeChars("Bye");
+        }
     }
 
-    int
+    //Returns null if didnt get result or get in wrong format
+    String[] executeList(String path) throws IOException {
+        String result = "";
+        try (var socket = new Socket(hostName, port);
+             var in = new BufferedReader(new InputStreamReader(
+                     new DataInputStream(socket.getInputStream()), StandardCharsets.UTF_8));
+             var out = new DataOutputStream(socket.getOutputStream())) {
+            var request = "1 " + path;
+            out.writeChars(request);
+            String input;
+            while ((input = in.readLine()) != null) {
+                result += input;
+            }
+        }
+
+        String[] res = result.split(" ");
+        if (res.length == 0) {
+            return null;
+        }
+
+        try {
+            if (Integer.parseInt(res[0]) == -1) {
+                return null;
+            }
+        } catch (NumberFormatException e) {
+            return null;
+        }
+
+        return res;
+    }
+
+    //returns
+    byte[] executeGet(String path) {
+        try (var socket = new Socket(hostName, port);
+             var in = new DataInputStream(socket.getInputStream());
+             var out = new DataOutputStream(socket.getOutputStream())) {
+            var request = "2 " + path;
+            out.writeChars(request);
+            int size = in.readInt();
+            if (size == -1) {
+                return null;
+            }
+            var buffer = new byte[size];
+            in.read(buffer);
+            if (in.read() != 0) {
+                return null;
+            }
+            return buffer;
+        } catch (IOException e) {
+            return null;
+        }
+
+    }
 }
